b1d4e36a1c6e654dc6e10b1e275e93fe6bcb53e2
Maxilo92 <Priesnitz.maximilian@web.de>
2025-02-27 13:23:26 +0100

    Update app.py
    Jetzt mit logout und session_id

--- Nutzerverwaltung/Final/app.py
+++ Nutzerverwaltung/Final/app.py
@@ -19,7 +19,6 @@ def db_connection():
         print("Fehler bei der Datenbankverbindung:", e)
         return None
 
-
 @app.route('/SignUp', methods=['GET', 'POST'])
 def register():
     message = ""
@@ -28,22 +27,18 @@ def register():
         passwort = request.form["password"]
         passwort_bestaetigen = request.form["password_be"]
 
-        # Überprüfen, ob das Passwort mindestens 4 Zeichen lang ist
         if len(passwort) < 4:
             message = "Das Passwort muss mindestens 4 Zeichen lang sein!"
             return render_template('SignUp.html', message=message)
 
-        # Überprüfen, ob der Benutzername mindestens 3 Zeichen lang ist
         if len(username) < 3:
             message = "Der Benutzername muss mindestens 3 Zeichen lang sein!"
             return render_template('SignUp.html', message=message)
         
-        # Überprüfen, ob der Benutzername maximal 20 Zeichen lang ist
         if len(username) > 20:
             message = "Der Benutzername darf maximal 20 Zeichen lang sein!"
             return render_template('SignUp.html', message=message)
 
-        # Überprüfen, ob der Benutzername nur Buchstaben und Zahlen enthält
         if not re.match("^[A-Za-z0-9]+$", username):
             message = "Der Benutzername darf nur Buchstaben und Zahlen enthalten!"
             return render_template('SignUp.html', message=message)
@@ -60,30 +55,28 @@ def register():
 
             cursor = connection.cursor()
 
-            # Überprüfen, ob der Benutzername bereits existiert
             cursor.execute("SELECT COUNT(*) FROM user_data WHERE Benutzername = %s", (username,))
             count = cursor.fetchone()[0]
             if count > 0:
                 message = "Der Benutzername ist bereits vergeben!"
                 return render_template('SignUp.html', message=message)
 
-            # Überprüfen, ob dies der erste Benutzer ist
             cursor.execute("SELECT COUNT(*) FROM user_data")
             user_count = cursor.fetchone()[0]
 
-            # Rolle festlegen: 2 für den ersten Benutzer, 0 für alle anderen
             rolle = 2 if user_count == 0 else 0
-            gruppe = None  # Gruppe ist immer Null
+            gruppe = None
 
-            # Passwort hashen
             hashed_passwort = bcrypt.hashpw(passwort.encode("utf-8"), bcrypt.gensalt())
 
-            # Benutzer in die Datenbank einfügen
             cursor.execute(
                 "INSERT INTO user_data (Benutzername, Passwort, Email, Gruppe, Rolle) VALUES (%s, %s, %s, %s, %s)",
                 (username, hashed_passwort, username + "@email.com", gruppe, rolle)
             )
             connection.commit()
+            cursor.execute("SELECT Benutzer_id FROM user_data WHERE Benutzername = %s", (username,))
+            user_id = cursor.fetchone()[0]
+            session['Benutzer_id'] = user_id
             message = "Registrierung erfolgreich!"
             return render_template('SignUp.html', message=message)
         except Exception as e:
@@ -108,7 +101,6 @@ def login():
         if connection:
             cursor = connection.cursor(dictionary=True)
             
-            # Überprüfen, ob der Benutzer gesperrt ist
             cursor.execute('SELECT * FROM user_data WHERE Benutzername = %s AND Sperren = 1', (username,))
             is_user_locked = cursor.fetchone()
             if is_user_locked:
@@ -117,28 +109,25 @@ def login():
                 connection.close()
                 return render_template('Login.html', message=message)
             
-            # Benutzerdaten abrufen
             cursor.execute('SELECT * FROM user_data WHERE Benutzername = %s', (username,))
             user = cursor.fetchone()
-            cursor.close()
-            connection.close()
 
             if user:
-                # Passwortüberprüfung mit bcrypt
                 if bcrypt.checkpw(password.encode('utf-8'), user['Passwort'].encode('utf-8')):
                     session['loggedin'] = True
-                    #session['user_id'] = user['id']
-                    session['username'] = user['Benutzername']
+                    session['Benutzer_id'] = user['Benutzer_id']
                     if user['Rolle'] == 2:
-                        return redirect(url_for('Dashboard.html')) # Hier das HTML vom Admin Dashboard
+                        return redirect(url_for('admin_dashboard'))  # Hier das HTML vom Admin Dashboard
                     elif user['Rolle'] == 1:
-                        return render_template('Dashboard.html')   # Hier das HTML vom Mitarbeiter Dashboard
+                        return render_template('employee_dashboard.html')   # Hier das HTML vom Mitarbeiter Dashboard
                     else:
-                        return render_template('Dashboard.html')   # Hier das HTML vom Gast Dashboard
+                        return render_template('guest_dashboard.html')   # Hier das HTML vom Gast Dashboard
                 else:
                     message = 'Falsches Passwort!'
             else:
                 message = 'Benutzer existiert nicht!'
+            cursor.close()
+            connection.close()
         else:
             message = 'Datenbankverbindung fehlgeschlagen!'
     
@@ -147,9 +136,8 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('loggedin', None)
-    session.pop('user_id', None)
-    session.pop('username', None)
-    return redirect(url_for('Login'))
+    session.pop('Benutzer_id', None)
+    return render_template('logout.html')
 
 @app.route('/index')
 def index():
