d6b35befb33f41928cf2073654967568290a689d
Sxphie8 <sophiehandy8@gmail.com>
2025-02-28 09:55:10 +0100

    Add files via upload

--- Dashboard/Dashboard.py
+++ Dashboard/Dashboard.py
@@ -9,7 +9,7 @@ def db_connection():
             host="host.docker.internal",
             user="root",
             password="1234",
-            database="Backend"
+            database="backend"
         )
         return connection
     except mysql.connector.Error as err:
@@ -22,17 +22,21 @@ def dashboard():
     try:
         connection = db_connection()
         cursor = connection.cursor()
-        cursor.execute("SELECT Ticket_id, Betreff, Benutzername, Beschreibung, Status, Prio, Team, Erstelldatum, Sprint \
-                       FROM ticket_data\
-                       INNER JOIN user_data \
-                       ON ticket_data.Benutzer_id = user_data.Benutzer_id")
+        cursor.execute('''
+                       SELECT ticket_data.Ticket_id, user_data.Benutzername, ticket_data.Betreff, ticket_data.Beschreibung, ticket_data.Status, ticket_data.Prio, ticket_data.Team, ticket_data.Erstelldatum, ticket_data.Sprint
+                       FROM ticket_data
+                       INNER JOIN user_data
+                       ON ticket_data.Benutzer_id = user_data.Benutzer_id;                    
+                       ''')
         ticket = cursor.fetchall()
-        return render_template("Ticket_Uebersicht.html", ticket=ticket)
+        
     except Exception as e:
         return f"Fehler: {e}"
     finally: 
         cursor.close()
         connection.close()
+
+        return render_template("Ticket_Uebersicht.html", ticket=ticket)
     
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
